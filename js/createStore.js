e0adcec9f49af32bba982cd20b07927d81d8d811
George <georuiz817@gmail.com>
2019-10-28 18:29:04 -0400

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,29 @@
-let state;
+// make our state a function so it's not global and can be altered somewhere else in the app..
+//So we want our dispatch method to call a reducer every time an action is dispatched so we call it in the arg
+function createStore(reducer) {
+  let state;
+ /* state is now accessible to dispatch by putting dispatch in our function..
+  we need to be able to call dispatch in other areas 
+  So we expose the method by having our function return a JavaScript object that has a dispatch method.
+  We'll call this returned JavaScript object our store, and, therefore, we'll call the 
+  method createStore, because that's what it does.
+ */
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  };
+ 
+
+  //This method simply returns the state, which we can use elsewhere in our application.
+  function getState() {
+    return state;
+  }
+ 
+  return {
+    dispatch,
+    getState
+  };
+};
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +35,29 @@ function reducer(state = { count: 0 }, action) {
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
 dispatch({ type: '@@INIT' })
 let button = document.getElementById('button');
-
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+ 
+button.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREASE_COUNT' });
 })
+
+
+//So we have this object called a store which contains all of our application's state
+// createStore takes the reducer reducer as an argument
+let store = createStore(reducer);
+store.dispatch({ type: '@@INIT' });
+
+
+//conclusion 
+//a call to dispatch should call a reducer, reassign the state, and render a change.
+//This is implemented inside the createStore method.
+
+
